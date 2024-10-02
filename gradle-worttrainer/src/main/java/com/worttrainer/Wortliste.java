1ceb951575db16fa5ba913b5ea33fa5a2c7811ae
tfischinger-tgm <tfischinger@students.tgm.ac.at>
2024-10-02 17:21:32 +0200

    generieren der Wortliste begonnen

---
+++ gradle-worttrainer/src/main/java/com/worttrainer/Wortliste.java
@@ -0,0 +1,49 @@
+package com.worttrainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Die Klasse Wortliste generiert die Liste für den Worttreiner
+ * @author Fischinger Tobias
+ * @param Wieviele Wörter der Wortliste hinzugefügt werden.
+ */
+
+public class Wortliste (int anzahl) {
+    String wort;
+    String url;
+    Double random;
+    boolean doppelt=true;
+    int einträge = 0;
+    String[] liste = new String[anzahl];
+    String pruefen;
+    String filename = "C:\Users\tobif\IdeaProjects\Worttrainer\gradle-worttrainer\src\Worttrainer.txt";
+    Path p = new Path(filename);
+    Scanner s = new Scanner(p);
+
+    //folgender Code ist zum Auslesen wieviele Einträge es insgesamt gibt in Worttrainer.txt.
+    while(s.hasnextLine()) {
+        einträge++;
+    }
+    String[] auslesen = new String[einträge];
+    for (int i = 0; i <einträge; i++ ) {
+        auslesen[i] = s.nextLine();
+    }
+    for (int i = 0; i <anzahl; i++) {
+        while (doppelt) {
+            random = (int) Math.random() * (einträge - 0) + 0;
+
+            for (int k = 0; k <liste.length; k++) {
+                if (liste[k].equals(auslesen[random])) {
+                    doppelt = true;
+
+                } else {
+                    doppelt = false;
+                }
+            }
+        }
+        liste[i] = auslesen[random];
+
+    }
+
+}
---
+++ gradle-worttrainer/src/main/java/com/worttrainer/Worttrainer.java
@@ -0,0 +1,3 @@
+package com.worttrainer;
+
+//Worttrainer wird hier implementiert
